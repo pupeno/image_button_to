467233065640773ceb204a1bbae65314e057062d
J. Pablo Fernández <pupeno@pupeno.com>
2009-11-24 08:35:10 +0100

    Initial commit to image_button_to.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'image_button_to'
+
+class Test::Unit::TestCase
+end
